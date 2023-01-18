51429bbb24f71c8e606766c5b46870b3806449c6
sallykhazaal <sallykhazaal@yahoo.com>
2023-01-18 09:30:45 -0500

    commit tree 6a5513d71425696cc323adcfb63b2390d081761a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(word) {return cats.push(word)}
+function destructivelyPrependCat(word) {return cats.unshift(word)}
+function destructivelyRemoveLastCat(word) {return cats.pop(word)}
+function destructivelyRemoveFirstCat(word) {return cats.shift(word)}
+function appendCat(word) {return [...cats, word]}
+function prependCat(word) {return [word, ...cats]}
+function removeLastCat(word) {return cats.slice(0,cats.length-1)}
+function removeFirstCat(word) {return cats.slice(1)}
